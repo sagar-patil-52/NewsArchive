bc6753aefdb6bdfd7e5dfcb3bbfb0d9dd19f2773
Sagar  Patil <patilsp52@gmail.com>
2019-06-05 16:46:41 +0530

    Added icons

---
+++ NewsArchives/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsArchives
+//
+//  Created by Sagar  Patil on 05/06/19.
+//  Copyright © 2019 Shaunak Jagtap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
